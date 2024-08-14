87fe3c4a605d6788fde0c416ffecfc0c188b15f6
JRLegarda <juansebastianlegarda@gmail.com>
2024-08-14 13:48:09 -0500

    Prueba 1

---
+++ src/main/java/model/Main.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private Scanner hola;
+
+    public static void main(String[] args) {
+         Scanner hola = new Scanner(System.in);
+
+        System.out.println("Hola como estas?");
+    }
+}
